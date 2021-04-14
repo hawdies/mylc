b3cbd4d1f7a8ebf872f629d7223a88bcd369b3c5
Hawdies <997520190@qq.com>
2021-04-14 14:27:17 +0800

    2020-04-14

---
+++ src/main/java/N201BitwiseAndOfNumbersRange.java
@@ -0,0 +1,21 @@
+/**
+ * 问题描述: 给你两个整数 left 和 right ，表示区间 [left, right] ，返回此区间内所有数字 按位与 的结果（包含 left 、right 端点
+ * <p>
+ * 解题思路: 按位与,只要有一个为0,则为0.
+ * 实质为求解公共前缀,后面补0.
+ *
+ * @author hawdies
+ * @date 2021/4/14
+ **/
+public class N201BitwiseAndOfNumbersRange {
+    public int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        while (left < right) {
+            shift++;
+            left >>>= 1;
+            right >>>= 1;
+        }
+        left <<= shift;
+        return left;
+    }
+}
