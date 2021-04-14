1f06f000c581d47067cad87e9cfe89d754c82058
Hawdies <hawdies@foxmail.com>
2021-04-14 19:19:17 +0800

    2020-04-14

--- src/main/java/N201BitwiseAndOfNumbersRange.java
+++ src/main/java/N201BitwiseAndOfNumbersRange.java
@@ -1,7 +1,7 @@
 /**
  * 问题描述: 给你两个整数 left 和 right ，表示区间 [left, right] ，返回此区间内所有数字 按位与 的结果（包含 left 、right 端点
  * <p>
- * 解题思路: 按位与,只要有一个为0,则为0.
+ * 解题思路: 按位与,只要有一个为0,则为0.(可以从简单例子分析其规律)
  * 实质为求解公共前缀,后面补0.<br>
  * <a href="https://leetcode-cn.com/problems/bitwise-and-of-numbers-range/solution/shu-zi-fan-wei-an-wei-yu-by-leetcode-solution/">
  * 详见题解</a>
