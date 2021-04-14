d0b93cff0fabfb29ca7e979b237f633ac62950b3
Hawdies <hawdies@foxmail.com>
2021-04-14 17:41:39 +0800

    2020-04-14

--- src/main/java/N201BitwiseAndOfNumbersRange.java
+++ src/main/java/N201BitwiseAndOfNumbersRange.java
@@ -3,6 +3,7 @@
  * <p>
  * 解题思路: 按位与,只要有一个为0,则为0.
  * 实质为求解公共前缀,后面补0.
+ * 详见https://leetcode-cn.com/problems/bitwise-and-of-numbers-range/solution/shu-zi-fan-wei-an-wei-yu-by-leetcode-solution/
  *
  * @author hawdies
  * @date 2021/4/14
